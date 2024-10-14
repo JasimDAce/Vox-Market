b78e153e0a49d753e67a5a4b3c7f318de8adc512
JasimDAce <mohdalijasim786@gmail.com>
2024-10-15 01:09:02 +0530

    I have made a New Design for signup

--- frontend/src/app/(main)/login/page.jsx
+++ frontend/src/app/(main)/login/page.jsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return <div>
+
+
+
+  </div>;
 };
 
 export default Login;
--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -20,6 +20,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
