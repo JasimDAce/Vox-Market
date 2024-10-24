5acb316526d7e70ad9ea00c609c2815661c37ffc
JasimDAce <mohdalijasim786@gmail.com>
2024-10-24 20:02:27 +0530

    I have created order model

--- backend/models/OrderModel.js
+++ backend/models/OrderModel.js
@@ -1,72 +1,72 @@
-const {Schema,model} = require('../connection');
+const { Schema, model, Types } = require("../connection");
 
 const CheckoutSchema = new Schema({
-    user: {
-    //   type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    items: [
-      {
-        product: {
+  user: {
+    type: Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+  items: [
+    {
+      product: {
         //   type: mongoose.Schema.Types.ObjectId,
-          ref: 'Product',
-          required: true,
-        },
-        quantity: {
-          type: Number,
-          required: true,
-          min: 1,
-        },
-        price: {
-          type: Number,
-          required: true,
-        },
-      },
-    ],
-    payment: {
-      method: {
-        type: String, // e.g., 'credit_card', 'paypal'
+        ref: "Product",
         required: true,
       },
-      status: {
-        type: String, // 'pending', 'completed', 'failed'
+      quantity: {
+        type: Number,
+        required: true,
+        min: 1,
+      },
+      price: {
+        type: Number,
         required: true,
       },
-      transactionId: {
-        type: String,
-      },
     },
-    shippingAddress: {
-      fullName: { type: String, required: true },
-      addressLine1: { type: String, required: true },
-      addressLine2: { type: String },
-      city: { type: String, required: true },
-      state: { type: String, required: true },
-      postalCode: { type: String, required: true },
-      country: { type: String, required: true },
-      phoneNumber: { type: String, required: true },
-    },
-    totalPrice: {
-      type: Number,
-      required: true,
-    },
-    taxAmount: {
-      type: Number,
-      required: true,
-    },
-    shippingCost: {
-      type: Number,
+  ],
+  payment: {
+    method: {
+      type: String, // e.g., 'credit_card', 'paypal'
       required: true,
     },
     status: {
-      type: String, // e.g., 'processing', 'shipped', 'delivered'
-      default: 'processing',
+      type: String, // 'pending', 'completed', 'failed'
+      required: true,
     },
-    createdAt: {
-      type: Date,
-      default: Date.now,
+    transactionId: {
+      type: String,
     },
-  });
+  },
+  shippingAddress: {
+    fullName: { type: String, required: true },
+    addressLine1: { type: String, required: true },
+    addressLine2: { type: String },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    postalCode: { type: String, required: true },
+    country: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  taxAmount: {
+    type: Number,
+    required: true,
+  },
+  shippingCost: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String, // e.g., 'processing', 'shipped', 'delivered'
+    default: "processing",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-  module.exports = model('Checkout', CheckoutSchema);
+module.exports = model("order", CheckoutSchema);
