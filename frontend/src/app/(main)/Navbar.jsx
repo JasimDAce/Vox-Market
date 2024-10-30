50a21c5f79a7cd937242fd9dbd5d0b15a97a4094
JasimDAce <mohdalijasim786@gmail.com>
2024-10-30 17:39:46 +0530

    I have changed the navbar

--- frontend/src/app/(main)/Navbar.jsx
+++ frontend/src/app/(main)/Navbar.jsx
@@ -19,7 +19,7 @@ export default function Navbar() {
   const { logout, loggedIn } = useAppContext();
 
   return (
-    <nav className="bg-[#F2EFE5] border-b border-gray-200">
+    <nav className="bg-[#F2EFE5]  border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
